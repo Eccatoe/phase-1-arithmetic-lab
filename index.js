29c20cf5787e6905d25c3a5f1533b6da39b31d6c
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-01-10 09:13:15 -0500

    commit tree ba7dbf5f236e66b3768cc4ef9f5d6a7927152f1b

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1=31;
+const num2=2;
+let multiply =  num1 * num2;
 
-// Write your code here
+let random=Math.ceil(Math.random());
+
+const num3=29;
+const num4=5;
+let mod= num3 % num4;
+
+let max=Math.max(0,16, 20);
